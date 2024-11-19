0bb5e2f231847980883cfe676e46cb605e9dc91a
VikramJagtap <vikramjagtap8880@gmail.com>
2024-11-19 11:47:32 +0530

    w

--- src/comp/Header.js
+++ src/comp/Header.js
@@ -38,6 +38,13 @@ function Header(props) {
               </ul>
             </div>
           </div>
+          <div className="switch">
+      <input type="checkbox" className="switch__input" id="Switch" />
+      <label className="switch__label" htmlFor="Switch">
+        <span className="switch__indicator"></span>
+        <span className="switch__decoration"></span>
+      </label>
+    </div>
           <div className={`form-check form-switch text-${props.mode === 'light' ? 'dark' : 'light'}`}>
             <input onClick={props.toggleMode} className="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault" />
             <label className="form-check-label" htmlFor="flexSwitchCheckDefault" style={{ display: "none" }}>{props.btntext}</label>
